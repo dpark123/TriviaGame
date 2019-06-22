d547cf7c354f5ed497e6a8a0b172387f18a9373a
Donald Park <donaldjpark@gmail.com>
2019-06-21 20:53:22 -0700

    restarted app and worked on it on separate files. got it to work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,78 +3,213 @@ var trivia = {
         title: "What is Spider-Man's true identity?",
         answers: [{
             option: "Tom Holland"
-            },{
-            option: "Andrew Garfield"
-            }, {
-            option: "Peter Parker",
-            }, {
-            option: "Tobey Maguire"}]
         }, {
+            option: "Andrew Garfield"
+        }, {
+            option: "Peter Parker",
+        }, {
+            option: "Tobey Maguire"
+        }]
+    }, {
         title: "In the comics, in what arc did Spider-Man reveal his identity to the world?",
         answers: [{
             option: "Civil War"
-            },{
-            option: "Secret Invasion"
-            }, {
-            option: "House of M",
-            }, {
-            option: "My name is Spider-Man"}]
         }, {
+            option: "Secret Invasion"
+        }, {
+            option: "House of M",
+        }, {
+            option: "My name is Spider-Man"
+        }]
+    }, {
         title: "In the comics, who was Peter's first girlfriend?",
         answers: [{
             option: "Mary Jane Watson"
-            },{
-            option: "Betty Brant"
-            }, {
-            option: "Carlie Cooper",
-            }, {
-            option: "Gwen Stacey"}]
         }, {
+            option: "Betty Brant"
+        }, {
+            option: "Carlie Cooper",
+        }, {
+            option: "Gwen Stacey"
+        }]
+    }, {
         title: "In the Superior Spider-Man, which super-villain took over Spider-Man's mind?",
         answers: [{
             option: "Doc Oc"
-            },{
+        }, {
             option: "Electro"
-            }, {
+        }, {
             option: "Green Goblin",
-            }, {
-            option: "Rhino"}]
+        }, {
+            option: "Rhino"
+        }]
     }]
 }
 
-$("#start").click(function(){
-    //alert("yeah");
-    $("#questionBody").html('');
-    questions();
+var questions = ["What is Spider-Man's true identity?",
+    "In the comics, in what arc did Spider-Man reveal his identity to the world?",
+    "In the comics, who was Peter's first girlfriend?",
+    "In the Superior Spider-Man, which super-villain took over Spider-Man's mind?"];
+
+var answers = [["Tom Holland", "Andrew Garfield", "Peter Parker", "Tobey Maguire"],
+["Civil War", "Secret Invasion", "House of M", "My name is Spider-Man"],
+["Mary Jane Watson", "Betty Brant", "Carlie Cooper", "Gwen Stacey"],
+["Doc Oc", "Electro", "Green Goblin", "Rhino"]
+];//answers: 2Peter Parker, 0Civil War, 1Betty Brant, 0Doc Oc
+
+var currentQuestion = 0;
+var answerKey = [2, 0, 1, 0];
+var currentAnswer = answerKey[currentQuestion];
+var guessesCorrect = 0;
+var seconds = 30;
+
+console.log(answers[1][2]);
+
+$(document).ready(function () {
+    $("#start").click(function () {
+        $("#start").hide();
+        $("#main").show();
+
+    })
+});
+
+
+$("#start").click(function () {
+    //intervalId = setInterval(count, 1000);
+    newQuestion();
+
+
 })
 
-var qNext = 0;
-var correctAns = 0;
-function questions (){
-    var timeR = $("<p></p>").text("Time Remaing: ");
-    var q = $("<p></p>").text(trivia.questions[qNext].title);
-    $("#questionBody").append(timeR, q);
-    for(var i = 0; i<4; i++){
-        var ans = $("<p></p>").text(trivia.questions[qNext].answers[i].option);
-        $("#questionBody").append(ans);
+
+
+function newQuestion() {
+
+    console.log(currentQuestion);
+    if (currentQuestion == 4) {
+        $("#main").hide();
+        resultScreen();
     }
-    qNext++;
-    if(qNext==5){
-    endGame();
+    else {
+        intervalId = setInterval(count, 1000);
+        seconds = 30;
+        $("#currentQuestion").show();
+        $("#results").hide();
+        $("#correctAnswer").hide();
+        $("#question").html(questions[currentQuestion]);
+        for (var i = 0; i < 4; i++) {
+            $("#a" + i).text(answers[currentQuestion][i]);
+        }
     }
+
 }
 
-var timerSet = setTimeout(timeUp, 30000);
+setTimeout(newQuestion, 35000);
 
-function timeUp(){
-    $("#questionBody").html("time is up");
+//$(document).ready(function () {
+$("#answerList").on('click', 'button', function (e) {
+    console.log("It worked");
+    var idClicked = e.target.id;
+    console.log(idClicked);
+    //if user guess correctly
+    if (idClicked === "a" + answerKey[currentQuestion]) {
+        //alert("YES!");
+        //newQuestion();
+        resultCorrect();
+        //clearInterval(intervalId);
+        guessesCorrect++;
+        // console.log(guessesCorrect);
+
+    }
+    //if user guess incorrectly
+    if (idClicked !== "a" + answerKey[currentQuestion]) {
+
+        resultIncorrect();
+        //clearInterval(intervalId);
+    }
+
+    currentQuestion++;
+    console.log(currentQuestion);
+    // newQuestion();
+
+});
+
+
+//});
+var intervalId;
+
+function resultTimeOut(e) {
+    if (e == 0) {
+        //resultTimeOut();
+        clearInterval(intervalId);
+        //seconds = 30;
+        $("#results").show();
+        $("#correctAnswer").show();
+        $("#status").html("Out of Time!");
+        $("#correctAnswer").html("The Correct Answer was: " + answers[currentQuestion][answerKey[currentQuestion]]);
+        currentQuestion++;
+        setTimeout(newQuestion, 5000);
+
+    }
+
 }
 
-function endGame(){
-    $("#questionBody").html(correctAns);
+function count() {
+    seconds--;
+    // $("#display").html(currentTime);
+    $("#timeRemaining").html(seconds);
+    // if (seconds == 0) {
+    //     clearInterval(intervalId);
+    //     seconds = 30;
+    // }
+    resultTimeOut(seconds);
+
+    //seconds--;
 }
 
-console.log(trivia.questions[0].answers[0].option);
-console.log(trivia.questions[1].answers[1].option);
-console.log(trivia.questions[2].answers[2].option);
-console.log(trivia.questions[3].answers[3].option);
+function resultScreen() {
+    $("#final").html("you got " + guessesCorrect + " out of " + questions.length + " questions correct");
+}
+
+
+function resultCorrect() {
+
+    clearInterval(intervalId);
+    $("#results").show();
+    $("#currentQuestion").hide();
+    $("#status").html("CORRECT!");
+    //currentQuestion++;
+    setTimeout(newQuestion, 5000);
+
+}
+
+function resultIncorrect() {
+    
+
+    clearInterval(intervalId);
+    $("#results").show();
+    $("#correctAnswer").show();
+    $("#status").html("INCORRECT");
+    $("#correctAnswer").html("The Correct Answer was: " + answers[currentQuestion][answerKey[currentQuestion]]);
+   // currentQuestion++;
+    setTimeout(newQuestion, 5000);
+
+
+}
+
+
+
+// function resultTimeOut(e){
+//     if (e === 0) {
+//         //resultTimeOut();
+//         clearInterval(intervalId);
+//         //seconds = 30;
+
+//     $("#results").show();
+//     $("#correctAnswer").show();
+//     $("#status").html("Out of Time!");
+//     $("#correctAnswer").html("The Correct Answer was: " + answers[currentQuestion][currentAnswer]);
+//     setTimeout(newQuestion, 5000);
+//     }
+// }
+$("#main").hide();
